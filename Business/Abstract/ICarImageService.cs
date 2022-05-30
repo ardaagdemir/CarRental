777588c84888a6a7ef67ff3c79a8f599c031bdc9
arda ağdemir <ardagdemirr@gmail.com>
2022-05-31 00:07:36 +0300

    31.05.2022

---
+++ Business/Abstract/ICarImageService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Core.Utilities.Results;
+using Entities.Concrete;
+using Entities.DTOs;
+
+namespace Business.Abstract
+{
+    public interface ICarImageService
+    {
+        IResult Add(CarImage carImage);
+        IResult Delete(CarImage carImage);
+        IResult Update(CarImage carImage);
+        IDataResult<List<CarImage>> GetAll();
+        IDataResult<List<CarImage>> GetByCarId(int carId);
+    }
+}
--- Core/Entities/IDto.cs
+++ Core/Entities/IDto.cs
@@ -5,13 +5,6 @@
     //Car class' ında bulunan "Id" ile Brand class'ında bulunan "BrandName" i bir arada göstermeye yarayan bir kod bloğudur.
     public interface IDto
     {
-        public int CarId { get; set; }
-        public string CarName { get; set; }
-        public string BrandName { get; set; }
-        public string ColorName { get; set; }
-        public int ModelYear { get; set; }
-        public decimal DailyPrice { get; set; }
-        public string Description { get; set; }
 
     }
 }
